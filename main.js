6130601ed77dcd6c38cacb313706e293a9e4849a
CristianSoare <kitcort@outlook.com>
2022-01-18 21:05:27 +0000

    Added loading state and took care of a few bugs

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
 const options = { method: 'GET', headers: { 'Accept': 'application/json'} };
+const apiLoadingState = document.querySelector('.loading');
 
 async function fetchData(event) {
+    apiLoadingState.classList += ' loading__active';
     let searchQuery = event.target.value
     let returnedData = await fetch(`https://api.opensea.io/api/v1/assets?owner=${searchQuery}&order_direction=asc&offset=0&limit=50`, options);
     let jsonData = await returnedData.json('');
-    
     console.log(jsonData)
     let userAssetsList = document.querySelector('.assets');
     userAssetsList.innerHTML = jsonData.assets.map((asset) => assetHTML(asset)).join('');
+    apiLoadingState.classList.remove("loading__active");
 }
 
 function assetHTML(asset) {
